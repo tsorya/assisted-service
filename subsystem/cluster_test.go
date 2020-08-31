286f735e1389acf0fa89e1f0d8f1c17793f9cded
Igal Tsoiref <itsoiref@redhat.com>
2020-08-31 22:56:26 +0300

    MGMT-2006 tests

--- subsystem/cluster_test.go
+++ subsystem/cluster_test.go
@@ -1,9 +1,11 @@
 package subsystem
 
 import (
+	"archive/tar"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -1046,6 +1048,38 @@ var _ = Describe("cluster install", func() {
 				Expect(err).NotTo(HaveOccurred())
 				Expect(s.Size()).ShouldNot(Equal(0))
 			}
+		})
+
+		It("[only_k8s]Download cluster logs", func() {
+			nodes := register3nodes(clusterID)
+			for _, host := range nodes {
+				kubeconfigFile, err := os.Open("test_kubeconfig")
+				Expect(err).NotTo(HaveOccurred())
+				_, err = agentBMClient.Installer.UploadHostLogs(ctx, &installer.UploadHostLogsParams{ClusterID: clusterID, HostID: *host.ID, Upfile: kubeconfigFile})
+				Expect(err).NotTo(HaveOccurred())
+				kubeconfigFile.Close()
+			}
+
+			file, err := ioutil.TempFile("", "tmp.zip")
+			Expect(err).NotTo(HaveOccurred())
+			defer file.Close()
+			_, err = userBMClient.Installer.DownloadClusterLogs(ctx, &installer.DownloadClusterLogsParams{ClusterID: clusterID}, file)
+			Expect(err).NotTo(HaveOccurred())
+			s, err := file.Stat()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(s.Size()).ShouldNot(Equal(0))
+			tarReader := tar.NewReader(file)
+			numOfarchivedFiles := 0
+			for {
+				_, err := tarReader.Next()
+				if err == io.EOF {
+					break
+				}
+				Expect(err).NotTo(HaveOccurred())
+				numOfarchivedFiles += 1
+				Expect(numOfarchivedFiles < len(nodes)+1).Should(Equal(true))
+			}
+			Expect(numOfarchivedFiles).Should(Equal(len(nodes)))
 		})
 
 		It("[only_k8s]Upload ingress ca and kubeconfig download", func() {
