886b2819e2241fbcfa88607812b40e1c7fe9e311
Igal Tsoiref <itsoiref@redhat.com>
2021-10-21 17:25:30 +0300

    Adding host monitor threshold per different functions

--- internal/host/host.go
+++ internal/host/host.go
@@ -33,6 +33,8 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+const monitoringThreshold = 0.4
+
 var BootstrapStages = [...]models.HostStage{
 	models.HostStageStartingInstallation, models.HostStageInstalling,
 	models.HostStageWritingImageToDisk, models.HostStageWaitingForControlPlane,
@@ -401,6 +403,7 @@ func (m *Manager) refreshRoleInternal(ctx context.Context, h *models.Host, db *g
 }
 
 func (m *Manager) refreshStatusInternal(ctx context.Context, h *models.Host, c *common.Cluster, i *common.InfraEnv, db *gorm.DB) error {
+	defer commonutils.MeasureOperationWithThresholdAndId("HostMonitoring-refreshStatusInternal", m.log, m.metricApi, monitoringThreshold, h.ID.String())()
 	if db == nil {
 		db = m.db
 	}
@@ -414,7 +417,11 @@ func (m *Manager) refreshStatusInternal(ctx context.Context, h *models.Host, c *
 	if err != nil {
 		return err
 	}
-	conditions, newValidationRes, err = m.rp.preprocess(vc)
+	func() {
+		defer commonutils.MeasureOperationWithThresholdAndId("HostMonitoring-preprocess", m.log, m.metricApi, monitoringThreshold, c.ID.String())()
+		conditions, newValidationRes, err = m.rp.preprocess(vc)
+	}()
+
 	if err != nil {
 		return err
 	}
@@ -434,13 +441,17 @@ func (m *Manager) refreshStatusInternal(ctx context.Context, h *models.Host, c *
 		}
 	}
 
-	err = m.sm.Run(TransitionTypeRefresh, newStateHost(h), &TransitionArgsRefreshHost{
-		ctx:               ctx,
-		db:                db,
-		eventHandler:      m.eventsHandler,
-		conditions:        conditions,
-		validationResults: newValidationRes,
-	})
+	func() {
+		defer commonutils.MeasureOperationWithThresholdAndId("HostMonitoring-TransitionTypeRefresh", m.log, m.metricApi, monitoringThreshold, c.ID.String())()
+		err = m.sm.Run(TransitionTypeRefresh, newStateHost(h), &TransitionArgsRefreshHost{
+			ctx:               ctx,
+			db:                db,
+			eventHandler:      m.eventsHandler,
+			conditions:        conditions,
+			validationResults: newValidationRes,
+		})
+	}()
+
 	if err != nil {
 		return common.NewApiError(http.StatusConflict, err)
 	}
@@ -982,6 +993,7 @@ func (m *Manager) ReportValidationFailedMetrics(ctx context.Context, h *models.H
 
 func (m *Manager) reportValidationStatusChanged(ctx context.Context, vc *validationContext, h *models.Host,
 	newValidationRes, currentValidationRes ValidationsStatus) {
+	defer commonutils.MeasureOperationWithThresholdAndId("HostMonitoring-reportValidationStatusChanged", m.log, m.metricApi, 0.5, h.ID.String())()
 	for vCategory, vRes := range newValidationRes {
 		for _, v := range vRes {
 			if currentStatus, ok := m.getValidationStatus(currentValidationRes, vCategory, v.ID); ok {
@@ -1021,6 +1033,7 @@ func (m *Manager) didValidationChanged(ctx context.Context, newValidationRes, cu
 }
 
 func (m *Manager) updateValidationsInDB(ctx context.Context, db *gorm.DB, h *models.Host, newValidationRes ValidationsStatus) (*common.Host, error) {
+	defer commonutils.MeasureOperationWithThresholdAndId("HostMonitoring-updateValidationsInDB", m.log, m.metricApi, monitoringThreshold, h.ID.String())()
 	b, err := json.Marshal(newValidationRes)
 	if err != nil {
 		return nil, err
